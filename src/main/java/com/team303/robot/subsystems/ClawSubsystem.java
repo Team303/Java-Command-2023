0dea60db192b340f1cc39360fb6e723589bebf1b
shamuz2006 <shamuz2006@gmail.com>
2023-01-19 16:03:18 -0500

    Yo

---
+++ src/main/java/com/team303/robot/subsystems/ClawSubsystem.java
@@ -0,0 +1,73 @@
+package com.team303.robot.subsystems;
+
+import edu.wpi.first.wpilibj.shuffleboard.ShuffleboardTab;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkMaxLowLevel.MotorType;
+import com.revrobotics.CANSparkMax.IdleMode;
+import com.revrobotics.RelativeEncoder;
+import com.team303.robot.util.GroundedDigitalInput;
+
+
+public class ClawSubsystem extends SubsystemBase {
+    /* ShuffleBoard */
+	public static final ShuffleboardTab CLIMBER_TAB = Shuffleboard.getTab("Climber");
+
+
+	private final CANSparkMax clawCanSparkMax = new CANSparkMax(19, MotorType.kBrushless);
+	private final RelativeEncoder clawEncoder;
+	public final GroundedDigitalInput outerLeft;
+	public final GroundedDigitalInput outerRight;
+	public final GroundedDigitalInput innerLeft;
+	public final GroundedDigitalInput innerRight;
+
+    private final int encoderGo;
+
+	public ClawSubsystem(int encoderRotations) 
+    {
+	clawCanSparkMax.setInverted(true);
+	clawCanSparkMax.setIdleMode(IdleMode.kBrake);
+	clawEncoder = clawCanSparkMax.getEncoder();
+
+	//limit switches
+	outerLeft = new GroundedDigitalInput(6);
+	outerRight = new GroundedDigitalInput(9);
+	innerLeft = new GroundedDigitalInput(15);
+	innerRight = new GroundedDigitalInput(22);
+
+    encoderRotations = encoder
+    
+    }
+
+		//claw motor getter among us imposter mode
+		public CANSparkMax getClawCanSparkMax() {
+			return clawCanSparkMax;
+		}
+	
+		//limits
+		public boolean outerLimitReached() {
+			return outerLeft.get() || outerRight.get();
+		}
+	
+		public boolean innerLimitReached() {
+			return innerRight.get() || innerLeft.get();
+		}
+
+        public void close(double speed) {
+		if ((outerLimitReached() && speed < 0) || (innerLimitReached() && speed > 0)) {
+			climbMotor.set(0);
+			return;
+		}
+		climbMotor.set(speed);
+	}
+
+		
+
+	@Override
+	public void periodic() 
+    {
+
+	}
+}
+
