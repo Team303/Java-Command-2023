152e855b15911436e166be67a817dd0588e4d029
Alan Jiang <74510449+Oajoeawfjo-Jioa@users.noreply.github.com>
2023-03-11 17:26:48 -0500

    fix: :bug: Fix ReachPoint end method

--- src/main/java/com/team303/robot/commands/arm/ReachPoint.java
+++ src/main/java/com/team303/robot/commands/arm/ReachPoint.java
@@ -8,33 +8,38 @@ import com.team303.robot.RobotMap.Arm;
 
 import edu.wpi.first.math.geometry.Translation3d;
 import edu.wpi.first.wpilibj2.command.CommandBase;
+import static com.team303.robot.commands.arm.DefaultIKControlCommand.cartesianStorage;
 
-public class 
-ReachPoint extends CommandBase {
-    public Translation3d cartesianStorage;
+public class ReachPoint extends CommandBase {
+    public Translation3d cartesianCoords;
 
     public ReachPoint(double x, double z) {
-        this.cartesianStorage = new Translation3d(x, 0, z);
+        this.cartesianCoords = new Translation3d(x, 0, z);
         addRequirements(arm);
     }
 
-    public ReachPoint(Translation3d cartesianStorage) {
-        this.cartesianStorage = cartesianStorage;
+    public ReachPoint(Translation3d cartesianCoords) {
+        this.cartesianCoords = cartesianCoords;
         addRequirements(arm);
     }
 
     @Override
     public void execute() {
     
-        // System.out.println(cartesianStorage.toString());
-        arm.reachEmbedded(cartesianStorage);
-        ArmSubsystem.armKinematics.updateEmbedded((float) cartesianStorage.getX(), (float) cartesianStorage.getZ());
-        Robot.arm.effectorRoot.setPosition((Arm.SIMULATION_OFFSET + 150)/Arm.SIMULATION_SCALE+cartesianStorage.getX(),Arm.SIMULATION_OFFSET/Arm.SIMULATION_OFFSET+cartesianStorage.getZ());
+        // System.out.println(cartesianCoords.toString());
+        arm.reachEmbedded(cartesianCoords);
+        ArmSubsystem.armKinematics.updateEmbedded((float) cartesianCoords.getX(), (float) cartesianCoords.getZ());
+        Robot.arm.effectorRoot.setPosition((Arm.SIMULATION_OFFSET + 150)/Arm.SIMULATION_SCALE+cartesianCoords.getX(),Arm.SIMULATION_OFFSET/Arm.SIMULATION_OFFSET+cartesianStorage.getZ());
     }
 
-    // @Override
-    // public boolean isFinished() {
-    //     return Math.abs(arm.getError()) < 3.0;
-    // }
+    @Override
+    public boolean isFinished() {
+        return Math.abs(arm.getError()) < 3.0;
+    }
+
+    @Override
+    public void end(boolean interrupted) {
+        cartesianStorage = new Translation3d(cartesianCoords.getX(), 0, cartesianCoords.getZ());
+    }
 
 }
--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -7,12 +7,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "team303-robot";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 142;
-  public static final String GIT_SHA = "b58baa6ecc47ad20de351810949299f4eb50fe1a";
-  public static final String GIT_DATE = "2023-03-11 17:16:02 EST";
+  public static final int GIT_REVISION = 143;
+  public static final String GIT_SHA = "145e99111c39b7907892843009bdcfe64d0fb0bf";
+  public static final String GIT_DATE = "2023-03-11 17:17:59 EST";
   public static final String GIT_BRANCH = "drivebase";
-  public static final String BUILD_DATE = "2023-03-11 17:17:07 EST";
-  public static final long BUILD_UNIX_TIME = 1678573027298L;
+  public static final String BUILD_DATE = "2023-03-11 17:23:55 EST";
+  public static final long BUILD_UNIX_TIME = 1678573435492L;
   public static final int DIRTY = 1;
 
   private BuildConstants(){}
