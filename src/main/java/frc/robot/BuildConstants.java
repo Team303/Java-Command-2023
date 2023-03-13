4b2cee8f5dc8b65a3ff2f99c8e86597b4d794a42
Alan Jiang <74510449+Oajoeawfjo-Jioa@users.noreply.github.com>
2023-03-13 16:25:26 -0400

    fix: change gear ratios

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -7,12 +7,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "team303-robot";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 146;
-  public static final String GIT_SHA = "74fb8a4073fa35831ff92c94fec5369255481450";
-  public static final String GIT_DATE = "2023-03-11 17:28:10 EST";
+  public static final int GIT_REVISION = 149;
+  public static final String GIT_SHA = "ccfe49f4450f4fd3c8afe3af15e9464b985e7b65";
+  public static final String GIT_DATE = "2023-03-13 20:35:40 EDT";
   public static final String GIT_BRANCH = "drivebase";
-  public static final String BUILD_DATE = "2023-03-11 18:45:48 EST";
-  public static final long BUILD_UNIX_TIME = 1678578348336L;
+  public static final String BUILD_DATE = "2023-03-13 16:19:58 EDT";
+  public static final long BUILD_UNIX_TIME = 1678738798346L;
   public static final int DIRTY = 1;
 
   private BuildConstants(){}
