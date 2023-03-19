c1794a8ed9f9845bd90115487a7f424a5d4df9c3
Alan Jiang <74510449+Oajoeawfjo-Jioa@users.noreply.github.com>
2023-03-18 23:44:49 -0400

    fix: reset to absolute

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -7,13 +7,13 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "team303-robot";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 175;
-  public static final String GIT_SHA = "304036b7a3cdd36af09afbea5ce2ff804d241cd3";
-  public static final String GIT_DATE = "2023-03-18 07:59:34 EDT";
-  public static final String GIT_BRANCH = "master";
-  public static final String BUILD_DATE = "2023-03-18 17:45:58 EDT";
-  public static final long BUILD_UNIX_TIME = 1679175958317L;
-  public static final int DIRTY = 1;
+  public static final int GIT_REVISION = 176;
+  public static final String GIT_SHA = "385f6943ec9253504bc308e6161f1b313ab799f7";
+  public static final String GIT_DATE = "2023-03-18 18:11:33 EDT";
+  public static final String GIT_BRANCH = "competitioncode";
+  public static final String BUILD_DATE = "2023-03-18 23:40:09 EDT";
+  public static final long BUILD_UNIX_TIME = 1679197209735L;
+  public static final int DIRTY = 0;
 
   private BuildConstants(){}
 }
