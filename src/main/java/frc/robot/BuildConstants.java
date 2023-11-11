25c59eb83a47a80840c541a846016ba53042aae4
Alan Jiang <74510449+Oajoeawfjo-Jioa@users.noreply.github.com>
2023-11-10 22:46:30 -0500

    Ninjago

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -7,12 +7,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "team303-robot";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 223;
-  public static final String GIT_SHA = "804187c25de3174ebcbdd25c47e9802bdff19ca6";
-  public static final String GIT_DATE = "2023-11-10 22:39:02 EST";
+  public static final int GIT_REVISION = 224;
+  public static final String GIT_SHA = "925190d549d5ed16e4693f2e9ec02b6fa6b22892";
+  public static final String GIT_DATE = "2023-11-10 22:42:33 EST";
   public static final String GIT_BRANCH = "arya_arav/BE";
-  public static final String BUILD_DATE = "2023-11-10 22:39:59 EST";
-  public static final long BUILD_UNIX_TIME = 1699673999923L;
+  public static final String BUILD_DATE = "2023-11-10 22:46:12 EST";
+  public static final long BUILD_UNIX_TIME = 1699674372517L;
   public static final int DIRTY = 0;
 
   private BuildConstants(){}
